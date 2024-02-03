6bfcc01cb5020ed5b295ce97de52d81ff624ce36
Eeewey <alexey.agban@gmail.com>
2024-02-03 18:21:50 +0700

    Initial Commit

---
+++ sosal/sosalApp.swift
@@ -0,0 +1,17 @@
+//
+//  sosalApp.swift
+//  sosal
+//
+//  Created by Лёша Агбан on 03.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct sosalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
